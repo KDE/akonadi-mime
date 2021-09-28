069e5e13ba8e14b2dad157466cf7eebcef6d01e1
Laurent Montel <montel@kde.org>
2021-09-28 07:56:48 +0200

    Add autotest

---
+++ autotests/addressattributetest.cpp
@@ -0,0 +1,37 @@
+/*
+    SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "addressattributetest.h"
+#include "addressattribute.h"
+#include <QTest>
+QTEST_MAIN(AddressAttributeTest)
+AddressAttributeTest::AddressAttributeTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void AddressAttributeTest::shouldHaveDefaultValues()
+{
+    Akonadi::AddressAttribute attr;
+    QVERIFY(attr.from().isEmpty());
+    QVERIFY(attr.to().isEmpty());
+    QVERIFY(attr.cc().isEmpty());
+    QVERIFY(attr.bcc().isEmpty());
+    QVERIFY(!attr.deliveryStatusNotification());
+}
+
+void AddressAttributeTest::shouldDeserializeValues()
+{
+    Akonadi::AddressAttribute attr;
+    attr.setFrom(QStringLiteral("from"));
+    attr.setTo({QStringLiteral("to")});
+    attr.setBcc({QStringLiteral("bcc")});
+    attr.setCc({QStringLiteral("cc")});
+    attr.setDeliveryStatusNotification(true);
+    const QByteArray ba = attr.serialized();
+    Akonadi::AddressAttribute result;
+    result.deserialize(ba);
+    QVERIFY(attr == result);
+}
---
+++ autotests/addressattributetest.h
@@ -0,0 +1,20 @@
+/*
+    SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class AddressAttributeTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AddressAttributeTest(QObject *parent = nullptr);
+    ~AddressAttributeTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+    void shouldDeserializeValues();
+};
