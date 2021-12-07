ce7b54f4dc389efe0f4504e78e3e861aa457b3a3
Laurent Montel <montel@kde.org>
2021-12-07 19:38:46 +0100

    GIT_SILENT: use reuse lint CI support

--- serializers/Messages.sh
+++ serializers/Messages.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: none
 $XGETTEXT *.cpp -o $podir/libakonadi-kmime5-serializer.pot
--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: none
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/libakonadi-kmime5.pot
