c67cfeb84fd873991c5e9deb4ddc3d5860bd2087
Laurent Montel <montel@kde.org>
2022-06-13 08:16:42 +0200

    Start to fix bug 453969.
    If we try to modify too many item it will failed

---
+++ src/markascommandhelper.cpp
@@ -0,0 +1,46 @@
+#include "markascommandhelper_p.h"
+
+#include "akonadi_mime_debug.h"
+#include <Akonadi/ItemModifyJob>
+
+using namespace Akonadi;
+MarkAsCommandHelper::MarkAsCommandHelper(QObject *parent)
+    : QObject{parent}
+{
+}
+
+MarkAsCommandHelper::~MarkAsCommandHelper() = default;
+
+void MarkAsCommandHelper::start()
+{
+    if (!mItemsToModify.isEmpty()) {
+        auto modifyJob = new Akonadi::ItemModifyJob(mItemsToModify, this);
+        modifyJob->setIgnorePayload(true);
+        modifyJob->disableRevisionCheck();
+        connect(modifyJob, &Akonadi::ItemModifyJob::result, this, &MarkAsCommandHelper::slotModifyItemDone);
+    } else {
+        emitResult(Akonadi::CommandBase::OK);
+        deleteLater();
+    }
+}
+
+const Akonadi::Item::List &MarkAsCommandHelper::itemsToModify() const
+{
+    return mItemsToModify;
+}
+
+void MarkAsCommandHelper::setItemsToModify(const Akonadi::Item::List &newItemsToModify)
+{
+    mItemsToModify = newItemsToModify;
+}
+
+void MarkAsCommandHelper::slotModifyItemDone(KJob *job)
+{
+    if (job && job->error()) {
+        qCDebug(AKONADIMIME_LOG) << " Error trying to set item status:" << job->errorText();
+        emitResult(Akonadi::CommandBase::Failed);
+    } else {
+        emitResult(Akonadi::CommandBase::OK);
+    }
+    deleteLater();
+}
---
+++ src/markascommandhelper_p.h
@@ -0,0 +1,33 @@
+/*
+    SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "akonadi-mime_export.h"
+#include "commandbase.h"
+#include <Akonadi/Item>
+#include <QObject>
+namespace Akonadi
+{
+class AKONADI_MIME_EXPORT MarkAsCommandHelper : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MarkAsCommandHelper(QObject *parent = nullptr);
+    ~MarkAsCommandHelper() override;
+
+    Q_REQUIRED_RESULT const Akonadi::Item::List &itemsToModify() const;
+    void setItemsToModify(const Akonadi::Item::List &newItemsToModify);
+
+    void start();
+Q_SIGNALS:
+    void emitResult(Akonadi::CommandBase::Result result);
+
+private:
+    void slotModifyItemDone(KJob *job);
+    Akonadi::Item::List mItemsToModify;
+};
+}
