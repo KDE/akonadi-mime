da8f8da1df7203820b9c6d192480dcf21005f97c
Laurent Montel <montel@kde.org>
2022-06-13 10:06:33 +0200

    Add missing license

--- src/markascommandhelper.cpp
+++ src/markascommandhelper.cpp
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
 #include "markascommandhelper_p.h"
 
 #include "akonadi_mime_debug.h"
