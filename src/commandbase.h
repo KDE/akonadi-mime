624c29189491a88e4a78eceeae5db0e7f16e573c
Laurent Montel <montel@kde.org>
2022-06-13 19:55:40 +0200

    GIT_SILENT: coding style

--- src/commandbase.h
+++ src/commandbase.h
@@ -19,7 +19,12 @@ public:
     explicit CommandBase(QObject *parent = nullptr);
     virtual void execute() = 0;
 
-    enum Result { Undefined, OK, Canceled, Failed };
+    enum Result {
+        Undefined,
+        OK,
+        Canceled,
+        Failed,
+    };
 
 Q_SIGNALS:
     void result(Akonadi::CommandBase::Result);
