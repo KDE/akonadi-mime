2e926aacbc7e2bed129446e90fd9063200411828
Alessandro Astone <ales.astone@gmail.com>
2023-12-21 14:41:09 +0100

    Fix the translation template version number (5->6)
    While this module does not need to be co-installable with its Qt5 version,
    change the version suffix for consistency.

--- serializers/Messages.sh
+++ serializers/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: none
-$XGETTEXT *.cpp -o $podir/libakonadi-kmime5-serializer.pot
+$XGETTEXT *.cpp -o $podir/libakonadi-kmime6-serializer.pot
--- src/Messages.sh
+++ src/Messages.sh
@@ -2,4 +2,4 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: none
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/libakonadi-kmime5.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/libakonadi-kmime6.pot
